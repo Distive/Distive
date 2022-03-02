ef8bf3ff283ef3ac238ed11d36d1fed93df42205
simdi <sim04ful@gmail.com>
2022-03-02 09:17:07 +0100

    notes

---
+++ build.sh
@@ -0,0 +1,5 @@
+# run tests
+# if pass, build canister code
+# copy .dfx/local/canisters/rust_hello/* to sdk/declarations 
+# modify sdk/declarations/canisters/rust_hello/index.js appropriately
+# build sdk
--- integration/tests.sh
+++ integration/tests.sh
@@ -1,6 +1,10 @@
 # Start DFX 
+dfx start --background
 # Deploy Canisters with clean state
+dfx deploy rust_hello -m reinstall
 # Run Unit Tests (Canisters)
+cargo test
 # Run Unit Tests (JS SDK)
 
 # Canister <-> SDK Integration Tests
+
