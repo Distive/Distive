e742e357d0ab4a66a95ad561a694b5e2bec52bc9
simdi <me@simdijinkins.com>
2022-05-13 19:16:25 +0100

    updated start script

--- start-dev.sh
+++ start-dev.sh
@@ -1,5 +1,8 @@
 dfx start --background
-dfx deploy rust_hello
-# generate mock data
+dfx_pid=$!
+echo "yes" | dfx deploy chat_canister --mode reinstall
 ./generate_mock_data.sh
-npm run --prefix src/js/examples/zonia-next-example dev
+npm run --prefix src/js/examples/distive-next-example dev
+sleep infinity
+kill $dfx_pid
+# generate mock data
