431ad3a024de81ed2214af2c8f67d291760b82af
simdi <me@simdijinkins.com>
2022-05-31 20:29:49 +0100

    added ledger and test files

--- start_dev.sh
+++ start_dev.sh
@@ -1,8 +1,9 @@
 dfx start --background
 dfx_pid=$!
+./start_dev_ledger.sh
 echo "yes" | dfx deploy chat_canister --mode reinstall
+echo "yes" | dfx deploy treasury_canister --mode reinstall
 # ./generate_mock_data.sh
 npm run --prefix src/js/examples/distive-next-example dev
 sleep infinity
 kill $dfx_pid
-# generate mock data
---
+++ start_dev_ledger.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+dfx identity new minter
+dfx identity use minter
+export MINT_ACC=$(dfx ledger account-id)
+
+dfx identity use default
+export LEDGER_ACC=$(dfx ledger account-id)
+
+dfx deploy ledger_canister --argument '(record {minting_account = "'${MINT_ACC}'"; initial_values = vec { record { "'${LEDGER_ACC}'"; record { e8s=100_000_000_000 } }; }; send_whitelist = vec {}})'
